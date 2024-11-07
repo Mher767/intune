cf6036a72ac1fccf1e4a7377e8570d6671fedb6f
Mher767 <115552627+Mher767@users.noreply.github.com>
2024-11-07 18:55:32 +0400

    Update intune.sh

--- intune.sh
+++ intune.sh
@@ -2,23 +2,23 @@
 
 UBUNTU_VERSION=$(lsb_release -c | awk '{print $2}')
 
-echo "Downloading Microsoft Edge..."
+echo "Downloading Microsoft Edge-----------------------------------------------------------------------------------------------------"
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_130.0.2849.68-1_amd64.deb
 
-echo "Installing Microsoft Edge..."
+echo "Installing Microsoft Edge-----------------------------------------------------------------------------------------------------"
 sudo dpkg -i microsoft-edge-stable_130.0.2849.68-1_amd64.deb
 
-echo "Installing prerequisites (curl, gpg)..."
+echo "Installing prerequisites (curl, gpg)-----------------------------------------------------------------------------------------------------"
 sudo apt update
 sudo apt install -y curl gpg
 
-echo "Downloading Microsoft GPG key..."
+echo "Downloading Microsoft GPG key-----------------------------------------------------------------------------------------------------"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.gpg
 
-echo "Installing Microsoft GPG key..."
+echo "Installing Microsoft GPG key-----------------------------------------------------------------------------------------------------"
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
 
-echo "Adding Microsoft repository for Ubuntu ${UBUNTU_VERSION}..."
+echo "Adding Microsoft repository for Ubuntu ${UBUNTU_VERSION}   -----------------------------------------------------------------------------------------------------"
 
 if [ "$UBUNTU_VERSION" == "jammy" ]; then
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" | sudo tee /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list
@@ -29,8 +29,8 @@ else
     exit 1
 fi
 
-echo "Updating APT package index..."
+echo "Updating APT package index -----------------------------------------------------------------------------------------------------"
 sudo apt update
 
-echo "Installing Intune Portal..."
+echo "Installing Intune Portal -----------------------------------------------------------------------------------------------------"
 sudo apt install -y intune-portal
